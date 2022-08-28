2e4c9a7af1d3179f7b9fec7ea8c2ae1950664bbb
Daniel Correa <dcorrealenis@gmail.com>
2022-08-28 11:59:38 -0500

    Cambio de la home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        <h1 className='text-xl font-semibold text-sky-700'>Esta es la Home</h1>
+    <div className='items-center flex flex-col'>
+        <h1 className='text-4xl font-semibold text-sky-700'>Bienvenido a FireShopping</h1>
+        <div className='shadow bg-sky-300 rounded-lg py-3 px-3 mt-5'>
+          <p>Aqui podras anotar tus tareas para que nunca mas se te olviden, ademas podras editarlas y gestionarlas como
+          quieras</p>
+        </div>
     </div>
   )
 }
