b9daaa27c9c4849b82617a579cdda0e6d10b06f2
Daniel Correa <dcorrealenis@gmail.com>
2022-08-31 16:11:29 -0500

    cambio del nombre de la pestaña

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <h1 className='text-4xl font-semibold text-sky-700'>Bienvenido a FireTasks</h1>
         <div className='shadow bg-sky-300 rounded-lg py-3 px-3 mt-5'>
           <p className='font-semibold text-center'>Bienvenido a la pagina web donde podras administrar tus tareas, en FireTasks podras añadir y eliminar tus tareas y asi 
-          tener un control sobre ellas para que que nunca te olvides de que debes hacer.</p>
+          tener un control sobre ellas para que nunca te olvides de que debes hacer.</p>
         </div>
     </div>
   )
